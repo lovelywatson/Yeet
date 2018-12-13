7fde5490e3a48f198f19c8589b35b5e046902eda
Sarah Durden <sarah.frame.harmony@gmail.com>
2018-12-13 15:31:38 -0600

    Update substrings.js

--- substrings.js
+++ substrings.js
@@ -1,35 +1,15 @@
+// Complete the twoStrings function below.
 function twoStrings(s1, s2) {
     let match = 0;
-    let first, second;
-    let str1 = Array.from(s1);
-    let str2 = Array.from(s2);
+    let alpha = ('abcdefghijklmnopqrstuvwxyz');
+    let str1 = s1.split('');
+    let str2 = s2.split('');
+    let alphabet = alpha.split('');
 
-    if (str1.length > str2.length) {
-        first = str2;
-        second = str1;
-    } else {
-        first = str1;
-        second = str2;
-    }
-    console.log("Str 1 " + s1 + " Str2 " + s2);
-    for (let i = 0; i < first.length; i++) {
-        for (let j = 0; j < second.length; j++) {
-            if (first[i] == second[j]) {
-                match++;
-                break;
-            }
-        }
-        if (match > 0) {
+    for (const letter in alphabet) {
+        if (str1.includes(alphabet[letter]) && str2.includes(alphabet[letter])) {
             return ("YES");
-            i = first.length;
-            break;
-        } 
-        
-         
+        }
     }
-    if (match == 0) {
-        return ("NO");
-    }
-    
-
+    return ("NO");
 }
